b165ac5a5192ff831b777627d6766ce2c835e451
terjeviken <terje.viken2@outlook.com>
2020-05-06 10:43:09 +0200

    just syncing

--- Source.cpp
+++ Source.cpp
@@ -7,15 +7,19 @@ int main() {
 
     std::ifstream midistream("organ.mid", std::ios::binary);    
 
-    if (midistream.is_open()) {
+    if ( midistream.is_open() ) {
+
         MidiFile midi(midistream);
-        // will pull all the notes-tracks and sort into a single
-        // vector sorted by start_time and print out the xxx first sorted elements
+
+        // will pull all the notes-tracks and sort into a single vector
+        // sorted by start_time and print out the xxx first sorted elements
         //midi_test_read(midi, 300); 
 
         WinMidiPlayer player(midi);
-        player.Play(); // no point in pause() rewind if in single thread
-    }
+        player.Play(); // no point in pause() resume() if in single thread. Go multithread or study async thingies better?
+
+
+
+    }	
 	
-	return 0;
 }
